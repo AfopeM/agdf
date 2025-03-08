5f69e2498a3f06f8ad223153198f145d8c2ae43e
Oluwatobi <afopematilukuro@gmail.com>
2025-03-08 12:47:36 -0500

    - **Updated** `MemberCard.tsx` to enhance layout and styling. - **Refined** `SectionHeader.tsx` for improved responsiveness and visual appeal.

--- src/components/Cards/MemberCard.tsx
+++ src/components/Cards/MemberCard.tsx
@@ -46,9 +46,9 @@ export function MemberCard({ name, image }: MemberCardProp) {
     >
       <Image
         fill
-        src={`/team/${image}.png`}
         alt={`${name}'s image`}
         className="object-cover"
+        src={`/team/${image}.png`}
       />
 
       <p className="brand-animate ring-brand absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-4 rounded bg-black/10 px-4 py-2 text-center text-sm text-white capitalize ring-2 backdrop-blur-xl lg:text-base">
--- src/components/Layout/SectionHeader.tsx
+++ src/components/Layout/SectionHeader.tsx
@@ -1,35 +1,47 @@
 interface SectionHeadingProp {
   title: string;
-  content?: string;
+  content?: string | null;
   isLight?: boolean;
   isReverse?: boolean;
 }
 
 export default function SectionHeader({
   title,
-  content,
+  content = null,
   isLight,
   isReverse,
 }: SectionHeadingProp) {
   const isLightStyling = isLight ? "text-white" : "text-black";
   return (
-    <div
-      className={`${isReverse ? "lg:flex-row-reverse" : "lg:flex-row"} mb-16 flex w-full flex-col items-center justify-between gap-6 text-center`}
-    >
-      <h2
-        className={`${isLightStyling} ${isReverse ? "lg:text-end" : "lg:text-start"} relative text-3xl font-bold capitalize lg:self-start lg:text-4xl`}
-      >
-        {title}
-        <span
-          className={`${isReverse ? "lg:right-0" : "lg:left-0"} bg-brand absolute -bottom-2 left-1/2 h-1 w-28 -translate-x-1/2 lg:-bottom-4 lg:translate-x-0`}
-        />
-      </h2>
-
-      <p
-        className={`${isReverse ? "lg:text-start" : "lg:text-end"} text-base text-gray-400 lg:max-w-xl lg:text-lg`}
-      >
-        {content}
-      </p>
-    </div>
+    <>
+      {content === null ? (
+        <h2
+          className={`${isLightStyling} relative mb-8 text-start text-3xl font-black uppercase lg:text-4xl`}
+        >
+          {title}
+          <span
+            className={`bg-brand absolute -bottom-2 left-0 h-1 w-28 lg:-bottom-4 lg:translate-x-0`}
+          />
+        </h2>
+      ) : (
+        <div
+          className={`${isReverse ? "lg:flex-row-reverse" : "lg:flex-row"} mb-16 flex w-full flex-col items-center justify-between gap-8 text-center`}
+        >
+          <h2
+            className={`${isLightStyling} ${isReverse ? "lg:text-end" : "lg:text-start"} relative text-3xl font-black uppercase lg:self-start lg:text-4xl`}
+          >
+            {title}
+            <span
+              className={`${isReverse ? "lg:right-0" : "lg:left-0"} bg-brand absolute -bottom-2 left-1/2 h-1 w-28 -translate-x-1/2 lg:-bottom-4 lg:translate-x-0`}
+            />
+          </h2>
+          <p
+            className={`${isReverse ? "lg:text-start" : "lg:text-end"} text-base text-gray-400 lg:max-w-xl lg:text-lg`}
+          >
+            {content}
+          </p>
+        </div>
+      )}
+    </>
   );
 }
